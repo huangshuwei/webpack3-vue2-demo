941ad999062c307cac0e2b1bd9bc8e4f7a049f71
黄书伟 <hsw89520@163.com>
2017-07-04 17:41:28 +0800

    添加进度条

--- app/index.js
+++ app/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import APP from './components/App.vue'
 import router from './router'
 /*import store from './store'*/
+import NProgress from 'nprogress'; // Progress 进度条
 
 import './css/index.css'
+import 'nprogress/nprogress.css';// Progress 进度条 样式
 
 
 import { Menu, Submenu,MenuItem,MenuItemGroup } from 'element-ui'
@@ -13,6 +15,18 @@ Vue.component(MenuItem.name, MenuItem)
 Vue.component(MenuItemGroup.name, MenuItemGroup)
 
 
+router.beforeEach((to, from, next) => {
+
+    NProgress.start();
+
+    next();
+
+    NProgress.done();
+})
+
+router.afterEach(() => {
+    NProgress.done(); // 结束Progress
+});
 
 new Vue({
         el: '#app',
@@ -20,3 +34,5 @@ new Vue({
         router,
         render: h => h(APP)
 })
+
+
